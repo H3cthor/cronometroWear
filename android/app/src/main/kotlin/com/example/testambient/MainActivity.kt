65088c6559af8a28d7f69bf5ccafd4ed26539f99
Hector Saldaña <hectorsalbe@hotmail.com>
2023-07-11 13:07:51 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/testambient/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.testambient
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:wear/wear.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+//////  Cronometro
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.compact,
+      ),
+      home: const WatchScreen(),
+    );
+  }
+}
+
+class WatchScreen extends StatelessWidget {
+  const WatchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return WatchShape(
+      builder: (context, shape, child) {
+        return AmbientMode(
+          builder: (context, mode, child) {
+            return TimerScreen(mode);
+          },
+        );
+      },
+    );
+  }
+}
+
+class TimerScreen extends StatefulWidget {
+  // const TimerScreen({super.key});
+  
+  final WearMode mode;
+
+  const TimerScreen(this.mode, {super.key});
+
+  @override
+  State<TimerScreen> createState() => _TimerScreenState();
+}
+
+class _TimerScreenState extends State<TimerScreen> {
+  late Timer _timer;
+  late int _count;
+  late String _strCount;
+  late String _status;
+
+  @override
+  void initState() {
+    _count = 0;
+    _strCount = "00:00:00";
+    _status = "Start";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: widget.mode == WearMode.active ? Colors.white : Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Center(
+              child: FlutterLogo(),
+            ),
+            const SizedBox(height: 4.0),
+            Center(
+              child: Text(
+                _strCount,
+                style: TextStyle(
+                    color: widget.mode == WearMode.active
+                        ? Colors.black
+                        : Colors.white),
+              ),
+            ),
+            _buildWidgetButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWidgetButton() {
+    if (widget.mode == WearMode.active) {
+      return Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          ElevatedButton(
+            // color: Colors.blue,
+            // textColor: Colors.white,
+            onPressed: () {
+              if (_status == "Start") {
+                _startTimer();
+              } else if (_status == "Stop") {
+                _timer.cancel();
+                setState(() {
+                  _status = "Continue";
+                });
+              } else if (_status == "Continue") {
+                _startTimer();
+              }
+            },
+            child: Text(_status),
+          ),
+          ElevatedButton(
+            // color: Colors.blue,
+            // textColor: Colors.white,
+            onPressed: () {
+              // ignore: unnecessary_null_comparison
+              if (_timer != null) {
+                _timer.cancel();
+                setState(() {
+                  _count = 0;
+                  _strCount = "00:00:00";
+                  _status = "Start";
+                });
+              }
+            },
+            child: const Text("Reset"),
+          ),
+        ],
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  void _startTimer() {
+    _status = "Stop";
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _count += 1;
+        int hour = _count ~/ 3600;
+        int minute = (_count % 3600) ~/ 60;
+        int second = (_count % 3600) % 60;
+        _strCount = hour < 10 ? "0$hour" : "$hour";
+        _strCount += ":";
+        _strCount += minute < 10 ? "0$minute" : "$minute";
+        _strCount += ":";
+        _strCount += second < 10 ? "0$second" : "$second";
+      });
+    });
+  }
+}
